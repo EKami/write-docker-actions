e24b5561903a4b1f5420602cd6b6fed7bab1d03a
Tuatini Godard <email@tuatini.me>
2021-03-16 11:34:45 +0000

    Change go file

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 )
 
 func main() {
 
-  // Access Inputs as environment vars
-  firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
-  secondGreeting := os.Getenv("INPUT_SECONDGREETING")
-  thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+// Access Inputs as environment vars
+firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+secondGreeting := os.Getenv("INPUT_SECONDGREETING")
+thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
 
-  // Use those inputs in the actions logic
-  fmt.Println("Hello " + firstGreeting)
-  fmt.Println("Hello " + secondGreeting)
+// Use those inputs in the actions logic
+fmt.Println("Hello " + firstGreeting)
+fmt.Println("Hello " + secondGreeting)
 
-  // Someimes inputs are not "required" and we can build around that
-  if thirdGreeting != "" {
+// Someimes inputs are not "required" and we can build around that
+if thirdGreeting != "" {
     fmt.Println("Hello " + thirdGreeting)
     }
 
